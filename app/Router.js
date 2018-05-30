4552f8ecf65b4c26c5dcc10e94554b971ad8e425
wolfing88 <573781175@qq.com>
2018-05-30 22:46:55 +0800

    first commit

---
+++ app/Router.js
@@ -0,0 +1,77 @@
+/**
+ * Created by Rabbit on 2017/11/3.
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View, BackHandler, StatusBar, DeviceEventEmitter } from 'react-native';
+import CardStackStyleInterpolator from 'react-navigation/src/views/CardStack/CardStackStyleInterpolator';
+
+import {
+  Scene,
+  Router,
+  Actions,
+  Reducer,
+  ActionConst,
+  Overlay,
+  Tabs,
+  Modal,
+  Drawer,
+  Stack,
+  Lightbox,
+} from 'react-native-router-flux';
+import { Theme } from 'teaset';
+
+import TabIcon from './Component/TabIcon';
+// import TabMiddleIcon from './Component/TabMiddleIcon'
+
+import Test from './Pages/Test';
+
+
+
+
+
+const reducerCreate = params => {
+  const defaultReducer = new Reducer(params);
+  return (state, action) => {
+    return defaultReducer(state, action);
+  };
+};
+
+const getSceneStyle = () => ({
+  backgroundColor: Theme.backgroundColor,
+
+});
+
+const onBackPress = () => {
+  console.log(Actions.state);
+  if (Actions.state.index !== 0) {
+    return false
+  }
+  Actions.pop()
+  return true
+}
+
+const router = (...props) => (
+  <Router createReducer={reducerCreate}
+          getSceneStyle={getSceneStyle}
+          backAndroidHandler={onBackPress}
+  >
+
+    <Modal
+      hideNavBar
+      transitionConfig={() => ({ screenInterpolator: CardStackStyleInterpolator.forFadeFromBottomAndroid })}
+    >
+
+      <Scene key={'home'} component={Test} />
+    </Modal>
+  </Router>
+);
+
+export default router;
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    backgroundColor: '#eee',
+    height:49,
+  },
+});
