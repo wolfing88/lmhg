d77fd9d6140125b05731226b83fa28a6e16524c4
wolfing88 <573781175@qq.com>
2018-06-02 12:04:12 +0800

    test

--- app/Component/CarouselView.js
+++ app/Component/CarouselView.js
@@ -15,20 +15,21 @@ export default class CarouselView extends Component {
     return (
       <Carousel
         delay={2000}   //自动切换的延迟 （毫秒）
-        style={{width: global.SCREEN_WIDTH, height: 200}}  //轮播组件的样式
+        style={{width: SCREEN_WIDTH, height: 200}}  //轮播组件的样式
         autoplay    //自动轮播
         pageInfo={false}    //在底部显示当前页面下标 / 页面个数
         swiper      //允许手势滑动
-
         bullets={true}  //显示小圆点
-        bulletStyle={{backgroundColor: '#fff', width: 5, height: 5}} //未选中时小圆点的样式
-        chosenBulletStyle={{backgroundColor: 'red', width: 5, height: 5}}//选中时小圆点的样式
+        bulletStyle={{backgroundColor: '#fff', width: px2dp(18), height: px2dp(18)}} //未选中时小圆点的样式
+        chosenBulletStyle={{backgroundColor: 'red', width: px2dp(18), height: px2dp(18)}}//选中时小圆点的样式
       >
-        {React.Children.map(['aa', 'bb', 'cc', 'bb', 'cc', 'bb', 'cc'], (child, index) => {
-          return (
-              <Image  style={{width: global.SCREEN_WIDTH, height: 200}}  source={{uri:'https://p.ssl.qhimg.com/dmsmfl/120_75_/t015fce4f5eb4863867.webp?size=200x200&phash=-836279116438999444'}}/>
-          )
-        })}
+        {
+          React.Children.map(this.props.imageList, (child, index) => {
+            return (
+              <Image  style={{width: SCREEN_WIDTH, height: 200}}  source={{uri:child}}/>
+            )
+          })
+        }
       </Carousel>
     );
   }
